314be49d933862c80917fca80036f87c444f5fd2
wraparica <77540266+wraparica@users.noreply.github.com>
2024-10-05 16:13:17 +0800

    commit message

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 //app.use(shopRoutes)
 app.use(indexRoutes)
 
-app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+
 
 app.listen(3000)
